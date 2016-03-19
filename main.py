abd4fee946e05dddc15d25b84c437125415870c8
madbastardd <bastardd666@gmail.com>
2016-03-19 19:32:39 +0200

    upgraded controller

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import view
+import user
+import decimal
 
 
 def main():
-    # declaration of a payments list
-    payments_list = []
+    # user declaration
+    main_user = user.User(200.00)
     # menu1
     while True:
         key = view.menu()
         # if statement to check what menu item was chosen
         if key == '1':
-            view.print_payments(payments_list)
+            view.print_user_info(main_user)
+            view.print_payments(main_user.get_payment_list())
         elif key == '2':
             account = view.input_accounting()
-            payments_list.append(account)
+            main_user.add_payment(account)
         elif key == '3':
             exit()
         else:
             print("You've entered incorrect value")
 
+
+decimal.getcontext().prec = 2
 main()
